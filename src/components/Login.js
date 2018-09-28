960acf669b35a7eb88b9185101a716e0da3383fa
trisjones <trisjones04@gmail.com>
2018-09-28 10:52:43 -0400

    Added some basic styling to mimic the main search page. Used BDD to ensure conditions work properly.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import App from './App';
+import { Link } from 'react-router-dom';
 const BASE_URL = 'http://localhost:3000/api/v1/users'
-  {/*   Responsibility
-      1. Is a class component that acts as a login form to send data[name] to the backend
-      2. Is the first page you see after opening our app
-      3. After submitting takes you to home page/route
-      4. Is its own route
-      */}
 
-const link = {
-  width: '50px',
-  padding: '12px',
-  margin: '0 200px 0',
-  background: 'black',
-  textDecoration: 'none',
-  color: 'white'
-}
+// const link = {
+//   width: '50px',
+//   padding: '12px',
+//   margin: '0 200px 0',
+//   background: 'black',
+//   textDecoration: 'none',
+//   color: 'white'
+// }
 
 class Login extends Component {
   state = {
-    input: ''
+    input: '',
+    displayError: false
   }
 
   handleInputChange = event => {
@@ -41,29 +35,40 @@ class Login extends Component {
           name: this.state.input
         })
       })
+      this.setState({ displayError: false })
+    } else {
+      this.setState({ displayError: true })
     }
   }
 
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>Create Your Profile</h1>
+        { this.state.displayError ? <h4>The name field can't be blank</h4> : null }
           <form>
             <input
+              className="login-input"
               onChange={this.handleInputChange}
               value={this.state.input}
               placeholder="Your name goes here">
             </input>
+            <Link
+              className="submit-button"
+              onClick={this.handleProfileCreation}
+              to={this.state.input !== '' ? "/weather" : "/"}
+              exact
+            >Submit</Link>
           </form>
-        <Link
-          onClick={this.handleProfileCreation}
-          to="/weather"
-          exact
-          style={link}
-        >Submit</Link>
       </div>
     );
   }
 }
 
 export default Login;
+
+// Responsibility
+//   1. Is a class component that acts as a login form to send data[name] to the backend
+//   2. Is the first page you see after opening our app
+//   3. After submitting takes you to home page/route
+//   4. Is its own route
