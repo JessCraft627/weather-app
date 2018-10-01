d96acbcc6ff5e7ca7914b91c93db2c52ca7e10d0
trisjones <trisjones04@gmail.com>
2018-10-01 16:04:59 -0400

    Used a setTimeout to delay fetch so that's now working but iterating through the data is proving to be complicated

--- src/components/City.js
+++ src/components/City.js
@@ -1,37 +1,47 @@
 import React, { Component } from 'react';
 import Buttons from './Buttons'
+const BASE_URL1 = '//api.openweathermap.org/data/2.5/forecast?q='
+const BASE_URL2 = '&units=Imperial&APPID=773d0a7cd6399fcc9e0901473a2796b0'
 
 class City extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      cityDetails: []
     }
   }
 
   componentDidMount = () => {
-    this.getSecondPart();
-    fetch('//api.openweathermap.org/data/2.5/forecast?q=' + this.state.searchTerm + '&units=Imperial&APPID=773d0a7cd6399fcc9e0901473a2796b0')
-    .then(r => r.json())
-    .then(data => console.log(data))
+    this.setSearchTermState();
+    this.delayFetch();
   }
 
-  getSecondPart = () => {
+  delayFetch = () => {
+    setTimeout(() => {
+      fetch(BASE_URL1 + this.state.searchTerm + BASE_URL2)
+      .then(r => r.json())
+      .then(data => this.setState({
+        cityDetails: data
+      }))
+    },1000)
+  }
+
+  setSearchTermState = () => {
     this.setState({
-      searchTerm: this.cityNameLogic()
+      searchTerm: this.props.location.search.split('=')[1]
     })
   }
 
-  cityNameLogic = () => {
-    const cityArray = this.props.location.search.split('=')[1]
-    console.log(cityArray)
-    return cityArray
+  mapTempDetails = () => {
+    if(this.state.cityDetails.length > 0) {
+      this.state.cityDetails.list.map(t=>t.main.temp_max)
+    }
   }
 
   render() {
-    console.log(this.state.searchTerm)
-    console.log('//api.openweathermap.org/data/2.5/forecast?q=' + this.state.searchTerm + '&units=Imperial&APPID=773d0a7cd6399fcc9e0901473a2796b0')
+    console.log(this.mapTempDetails())
     return (
       <div>
         <h2>City Page</h2>
