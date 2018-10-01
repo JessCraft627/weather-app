58ef4bac441ea4d0c43b6402eb651d261cf80e36
Jessicalong627 <jessicalong627@gmail.com>
2018-10-01 17:38:52 -0400

    new styles

--- src/components/City.js
+++ src/components/City.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
 import Buttons from './Buttons'
+const BASE_URL1 = '//api.openweathermap.org/data/2.5/forecast?q='
+const BASE_URL2 = '&APPID=773d0a7cd6399fcc9e0901473a2796b0&units=imperial'
 
+class City extends React.Component {
+  constructor(props) {
+    super(props);
 
-const City = (props) => {
-    console.log(props)
+    this.state = {
+      searchTerm: '',
+      cityDetails: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.setSearchTermState();
+    this.delayFetch();
+  }
+
+  delayFetch = () => {
+    setTimeout(() => {
+      fetch(BASE_URL1 + this.state.searchTerm + BASE_URL2)
+      .then(r => r.json())
+      .then(data => this.setState({
+        cityDetails: data.list
+      }))
+    },100)
+  }
+
+  setSearchTermState = () => {
+    this.setState({
+      searchTerm: this.props.location.search.split('=')[1]
+    })
+  }
+
+  render() {
+    console.log(this.state.cityDetails)
     return (
+      this.state.cityDetails.length > 0
+      ?
       <div>
-      
+        <h4>Day: {this.state.cityDetails[6].dt_txt}</h4>
+        <p>Max Temp: {this.state.cityDetails[6].main.temp_max}</p>
+        <p>Min Temp: {this.state.cityDetails[6].main.temp_min}</p>
+
+        <h4>Day: {this.state.cityDetails[14].dt_txt}</h4>
+        <p>{this.state.cityDetails[14].main.temp_max}</p>
+
+        <h4>Day: {this.state.cityDetails[22].dt_txt}</h4>
+        <p>{this.state.cityDetails[22].main.temp_max}</p>
+
+        <h4>Day: {this.state.cityDetails[30].dt_txt}</h4>
+        <p>{this.state.cityDetails[30].main.temp_max}</p>
+
+        <h4>Day: {this.state.cityDetails[38].dt_txt}</h4>
+        <p>{this.state.cityDetails[38].main.temp_max}</p>
       </div>
+      :
+      <h1>{'Loading...'}</h1>
     )
   }
 
-
+}
 
 export default City;
--- src/components/CityList.js
+++ src/components/CityList.js
@@ -11,49 +11,17 @@ import City from './City'
 class CityList extends Component  {
 
 
-  constructor(props) {
-    super(props);
-    this.state = {
-       toDashboard: false,
-       individualCity: [],
-       city: ''
-
-    }
-  }
-
- // handleClick = (e) => {
- //   this.setState(() => ({
- //       toDashboard: true
- //     }))
- // }
-
-
- handleCitiesWeather = (indCity) => {
-  fetch(`//api.openweathermap.org/data/2.5/forecast?q=${indCity}&units=Imperial&APPID=773d0a7cd6399fcc9e0901473a2796b0`)
-  .then(r => r.json())
-  .then(data => this.setState({individualCity: [data]}))
-}
-
-handleCityValue = (e) => {
-  e.preventDefault()
-  console.log(e.target.innerText)
-  let indCity = e.target.innerText
-  this.handleCitiesWeather(indCity)
-  this.setState({city:' '});
-}
-
   render() {
-    // if (this.state.toDashboard === true) {
-    //   return <Redirect to='/city' />
-    // }
-
     return (
-    <div className="city-list" onClick={this.handleCityValue} >
-        <h2  className="name"> {this.props.name}</h2>
+    <div className="city-list">
+        <h2  className="name"> <Link to={{
+                    pathname: '/city',
+                    search: `?query=${this.props.name.toLowerCase()}`
+                  }}>{this.props.name}</Link></h2>
         <span > <img className="weather-icon" src={`http://openweathermap.org/img/w/${this.props.icon}.png`} /> </span>
         <p className="temp"> {Math.round(this.props.temp)} F</p>
         <span className="desc"> {this.props.descr}</span>
-        <City datas={this.state.individualCity} />
+
     </div>
 
   )
