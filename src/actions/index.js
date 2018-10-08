52863fdbfc7e83bb433c8b1fb680de2e83f608da
trisjones <trisjones04@gmail.com>
2018-10-08 16:44:36 -0400

    Fixed small merge conflict bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,1 @@
 export const LOGIN_USER = 'LOGIN_USER'
-export const ADD_USER = 'ADD_USER'
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import {combineReducers} from 'redux'
-import { ADD_USER } from './../actions'
 import name from './name'
 
 export default combineReducers({
